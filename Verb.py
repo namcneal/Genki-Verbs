56441d3538d1a6c2770d393146a461edada9ccd5
rahrah997 <umichsjs@umich.edu>
2017-03-07 22:02:22 -0500

    Add files via upload

---
+++ Verb.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+class Verb(object):
+    def __init__(self, dictionary,kanji, group,meaning):
+        self.dict = dictionary
+        self.kanji = kanji
+        self.group = group
+        self.meaning = meaning
+
+    def potential(self):
+        # Ichidan
+        if self.group == "ru":
+            return self.dict[:-1] + u"られる"
+
+        #Godan
+        elif self.group == 'u':
+            ending = self.dict[-1]
+
+            all_endings = {u"う" : u"える",
+                           u"つ" : u"てる",
+                           u"る" : u"れる",
+                           u"む" : u"める",
+                           u"ぶ" : u"べる",
+                           u"ぬ" : u"ねる",
+                           u"す" : u"せる",
+                           u"く" : u"ける",
+                           u"ぐ" : u"げる"}
+
+            return self.dict[:-1] + all_endings[ending]
+                           
+
+        # Irregular
+        else:
+            all_possible = {u"いく" : u"いける",
+                            u"する" : u"できる",
+                            u"くる" : u"こられる"
+                            }
+            return all_possible[self.dict]
+
+    def passive(self):
+        # Ichidan
+        if self.group == "ru":
+            return self.dict[:-1] + u"られる"
+
+        #Godan
+        elif self.group == 'u':
+            ending = self.dict[-1]
+
+            all_endings = {u"う" : u"われる",
+                           u"つ" : u"たれる",
+                           u"る" : u"られる",
+                           u"む" : u"まれる",
+                           u"ぶ" : u"ばれる",
+                           u"ぬ" : u"なれる",
+                           u"す" : u"される",
+                           u"く" : u"かれる",
+                           u"ぐ" : u"がれる"}
+
+            return self.dict[:-1] + all_endings[ending]
+                           
+
+        # Irregular
+        else:
+            all_possible = {u"いく" : u"いかれる",
+                            u"する" : u"される",
+                            u"くる" : u"こられる"
+                            }
+            return all_possible[self.dict]
+
+
+
