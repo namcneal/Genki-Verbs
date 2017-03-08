92fb927861252876c90f5c6e34a4164e349fb5b6
Noah McNeal <namcneal@umich.edu>
2017-03-07 21:37:05 -0500

    Renamed the verb list used for testing. Forgot to commit updated files.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+# -*- coding: utf-8 -*-
 from Verb import Verb
+import FileIO
+
+
+test_list = FileIO.import_verb_list('testing_verb_list.csv')
+test_verbs = list()
+
+for row in test_list:
+    test_verbs.append(Verb(row[0], row[1], row[2], row[3]))
+
+for verb in test_verbs:
+    print verb.potential()
