f7d82ba7e3ecf16cde66454c014d9d9da8ece071
Noah McNeal <namcneal@umich.edu>
2017-03-19 23:48:53 -0400

    Redid the verb lists

--- Verbs/make_empty_files.py
+++ Verbs/make_empty_files.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 import unicodecsv as csv, codecs, cStringIO
 
-
-
 for num in range(3,25):
     new = open("genki_verbs_chapter_%d.csv" %num, "w+")
     writer = csv.writer(new)
-    writer.writerow([u"﻿Dictionary",u"Kanji",u"Type",u"Meaning"])
+    writer.writerow([u"﻿DictionaryKanji",u"DictionaryKana",u"Type",u"Meaning"])
     writer.writerow([u"行く",u"いく",u"u",u"to go%d" %num])
     writer.writerow([u"帰る",u"かえる",u"u",u"to return%d" %num])
     writer.writerow([u"食べる",u"たべる",u"ru",u"to eat%d" %num])
